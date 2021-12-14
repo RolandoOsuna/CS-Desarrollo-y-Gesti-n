599c7209d146c5573d3231dfc718f42c7a2b03fc
Rolando <rolando_osuna@live.com.mx>
2021-12-14 13:16:49 -0700

    Archivos

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="mx">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/IMG/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
+    <link rel="stylesheet" href="estilos.css">
+    <title>Inicio</title>
+</head>
+<body>
+    <header class="row align-items-center bg-header margen">
+        <div class="  ">
+            <div class="col text-center">
+                <img class="img-fluid " src="/IMG/item.png" width="300px">
+            </div>
+            <div class="col">
+                <h2 class="text-center titulo pb-5">CS DESARROLLO Y GESTION</h2>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
