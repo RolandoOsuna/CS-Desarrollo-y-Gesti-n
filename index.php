0f73bc0152a4c228b41c51861b5718273a093795
Rolando <rolando_osuna@live.com.mx>
2021-12-15 01:44:15 -0700

    items

--- index.php
+++ index.php
@@ -9,16 +9,90 @@
     <link rel="stylesheet" href="estilos.css">
     <title>Inicio</title>
 </head>
-<body>
+<body >
     <header class="row align-items-center bg-header margen">
-        <div class="  ">
+        <div>
             <div class="col text-center">
                 <img class="img-fluid " src="/IMG/item.png" width="300px">
             </div>
             <div class="col">
-                <h2 class="text-center titulo pb-5">CS DESARROLLO Y GESTION</h2>
+                <h2 class="text-center titulo-header pb-5">CS DESARROLLO Y GESTION</h2>
             </div>
         </div>
     </header>
+    <section class="bg-section py-5">
+        <div class="container">
+            <div class="row margen">
+                <div class="col-12 text-center">
+                    <h1 class="h3 titulo-section1">CS Desarrollo y Gestión</h1>
+                    <hr class="" style="text-align:center;margin:auto;" width="30%">
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p class="fs-5 text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae animi explicabo numquam necessitatibus praesentium repudiandae quod deserunt!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-trazo">
+        <div class="row margen  ">
+            <div class="d-md-flex ">
+                <div class="col-12 col-md-6 col-lg-5 text-center p-md">
+                    <h1 class=" titulo-section1">Lorem ipsum dolor</h1>
+                    <p class="fs-2 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, quas labore nobis ducimus pariatur doloremque.</p>
+                </div>
+                <div class="col-12 col-md-6 col-lg-7 text-center d-flex">
+                    
+                    <img class="img-fluid mx-auto" src="/IMG/economico.png" width="300px">
+                </div> 
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-gradient">
+        <div class="container">
+            <div class="row text-center">
+                <h1 class="h3 texto-card pt-3">Lorem, ipsum dolor.</h1>
+                <p class="fs-4 titulo-card">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed, adipisci.</p>
+                <hr>
+            </div>
+            <div class="row">
+                <div class="col text-center">
+                    <img src="/IMG/items blanco y nego/item-cohete.png" width="100px" alt="">
+                    <h1 class="h4 text-white text-shadow">Lorem ipsum</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi sequi perspiciatis nobis.</p>
+                </div>
+                <div class="col text-center">
+                    <img src="/IMG/items blanco y nego/item-pagina.png" width="100px" alt="">
+                    <h1 class="h4 text-white text-shadow">Lorem ipsum</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi sequi perspiciatis nobis.</p>
+                </div>
+                <div class="col text-center">
+                    <img src="/IMG/items blanco y nego/item-idea.png" width="100px" alt="">
+                    <h1 class="h4 text-white text-shadow">Lorem ipsum</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi sequi perspiciatis nobis.</p>
+                </div>
+            </div>
+            <div class="row pt-3">
+                <div class="col text-center">
+                    <img src="/IMG/items blanco y nego/item-servicio-cliente.png" width="100px" alt="">
+                    <h1 class="h4 text-white text-shadow">Lorem ipsum</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi sequi perspiciatis nobis.</p>
+                </div>
+                <div class="col text-center">
+                    <img src="/IMG/items blanco y nego/item-grafica.png" width="100px" alt="">
+                    <h1 class="h4 text-white text-shadow">Lorem ipsum</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi sequi perspiciatis nobis.</p>
+                </div>
+                <div class="col text-center">
+                    <img src="/IMG/items blanco y nego/item-ads.png" width="100px" alt="">
+                    <h1 class="h4 text-white text-shadow">Lorem ipsum</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi sequi perspiciatis nobis.</p>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
